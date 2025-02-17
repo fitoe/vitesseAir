0e0cbbd1d0cde8b570d75ce22197e294b52c5fe7
纪中庆 <imjzq@qq.com>
2025-02-17 11:03:24 +0800

    fix: pinia

--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 // Setup Pinia
 // https://pinia.vuejs.org/
-export const install: UserModule = ({ app, isClient, router, initialState, app }) => {
+export const install: UserModule = ({ isClient, initialState, app }) => {
   const pinia = createPinia()
   app.use(pinia)
 
@@ -11,14 +11,6 @@ export const install: UserModule = ({ app, isClient, router, initialState, app }
     initialState.pinia = pinia.state.value
   else
     pinia.state.value = initialState.pinia || {}
-
-  // router.beforeEach((to, from, next) => {
-  //   const store = useRootStore(pinia)
-  //   if (!store.ready)
-  //     // perform the (user-implemented) store action to fill the store's state
-  //     store.initialize()
-  //   next()
-  // })
 
   // Refer to
   // https://github.com/antfu/vite-ssg/blob/main/README.md#state-serialization
