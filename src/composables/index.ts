c099586d69c3e0c97bfaf3cd312e3f706283297e
纪中庆 <imjzq@qq.com>
2025-10-15 15:39:38 +0800

    feat: update dict

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,8 +1,2 @@
 export const setting = () => useSettingStore()
-const dictstore = useDictStore()
 export const user = useUserStore()
-
-export const dict = dictstore.dict
-export const _dict = dictstore._dict
-export const dictSync = dictstore.dictSync
-export const _dictSync = dictstore._dictSync
